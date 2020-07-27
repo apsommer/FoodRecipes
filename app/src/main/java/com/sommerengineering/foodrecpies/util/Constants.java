cb9388269838cd720335dadd1140f85119a552b0
Drew Sommer <drew.sommer@sulzer-us.com>
2020-07-27 14:55:37 -0700

    feat: Create recipe model

--- app/src/main/java/com/sommerengineering/foodrecpies/util/Constants.java
+++ app/src/main/java/com/sommerengineering/foodrecpies/util/Constants.java
@@ -2,7 +2,7 @@ package com.sommerengineering.foodrecpies.util;
 
 public class Constants {
 
-    public static final String BASE_URL = "https://recipesapi.herokuapp.com/api";
-    public static final String INGREDIENTS = "/get?rId=";
-    public static final String RECIPES = "/search?q=";
+    public static final String BASE_URL = "https://recipesapi.herokuapp.com";
+    public static final String INGREDIENTS = "/api/get?rId=";
+    public static final String RECIPES = "/api/search?q=";
 }
